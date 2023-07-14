6d79361466e143cf52ed6a253db9c8e2c471b6b6
Samir2871996 <samirmostafa1996@gmail.com>
2023-07-14 08:41:51 -0700

    second c task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c function that prints the int N.O less than 10
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+
+int a = 0;
+
+
+while (a <= 10)
+{
+printf("%d", a);
+a++;
+}
+putchar('\n');
+return (0);
+}
