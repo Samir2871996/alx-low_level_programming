fdaeb4735028ecb9855695812918ce4e00ca7fb0
Samir2871996 <samirmostafa1996@gmail.com>
2023-07-14 09:20:58 -0700

    second c task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c function that prints the int N.O less than 10
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+
+int a = '0';
+
+
+while (a <= '9')
+{
+putchar(a);
+putchar(',');
+putchar(' ');
+a++;
+}
+putchar('\n');
+return (0);
+}
