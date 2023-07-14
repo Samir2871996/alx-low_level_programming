77a86cb80d4e84ee930368bf7fd57a1c936290a3
Samir2871996 <samirmostafa1996@gmail.com>
2023-07-14 09:50:41 -0700

    second c task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,16 @@ int main(void)
 int a = '0';
 
 
-while (a <= '8')
+while (a <= '9')
 {
 putchar(a);
+if (a != '9')
+{
 putchar(',');
 putchar(' ');
+}
 a++;
 }
-putchar(a);
+putchar('\n');
 return (0);
 }
