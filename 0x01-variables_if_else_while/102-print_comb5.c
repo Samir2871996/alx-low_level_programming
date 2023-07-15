87999bbf96111062ae39e723d4ee4b45cf8bf208
Samir2871996 <samirmostafa1996@gmail.com>
2023-07-14 20:04:17 -0700

    samir

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,57 +12,42 @@
 
 int main(void)
 {
-
 	int a = '0';
 	int b = '0';
 	int c = '0';
 	int d = '0';
 
-
-
 	while (a <= 57)
 	{
-
 		while (b <= 57)
 		{
-
 			c = a;
 			d = b + 1;
-
 			while (c <= 57)
 			{
 				while (d <= 57)
 				{
-
 					putchar(a);
 					putchar(b);
 					putchar(' ');
 					putchar(c);
 					putchar(d);
-
 					if ((a != 57) | (b != 56) | (c != 57) | (d != 57))
 					{
-
 						putchar(',');
 						putchar(' ');
 					}
-
 					d++;
 				}
-
 				d = '0';
 				c++;
 			}
-
 			b++;
 		}
-
 		b = '0';
 		a++;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
 
