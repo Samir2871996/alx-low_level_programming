ad87761e184faf4518bd27d2de45e1bbefcbac96
Samir2871996 <samirmostafa1996@gmail.com>
2023-07-14 19:28:24 -0700

    samir

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c function that prints all possible
+ * combination of two two digit numbers
+ *
+ * Return: Always 0 (success)
+*/
+
+
+int main(void)
+{
+
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+
+
+
+	while (a <= '9')
+	{
+
+		while (b <= '9')
+		{
+
+			c = a;
+			d = b + 1;
+
+			while (c <= '9')
+			{
+				while (d <= '9')
+				{
+
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					putchar(d);
+
+					if (a != '9' | b != '8' | c != '9' | d != '9')
+					{
+
+						putchar(',');
+						putchar(' ');
+					}
+
+					d++;
+				}
+
+				d = 0;
+				c++;
+			}
+
+			b++;
+		}
+
+		b = 0;
+		a++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
+
