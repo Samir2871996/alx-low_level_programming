865640752b42166cfbc8c091ecb0edef6154345f
Samir2871996 <samirmostafa1996@gmail.com>
2023-07-18 07:10:02 -0700

    jhdjshjsdhj

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,16 +15,27 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-return (1);
+_putchar(',');
+_putchar(' ');
+_putchar('1');
+_putchar('\n');
 }
 else if (n == 0)
 {
 _putchar('0');
-return (0);
+_putchar(',');
+_putchar(' ');
+_putchar('0');
+_putchar('\n');
 }
 else  if (n < 0)
 {
 _putchar('-');
-return (-1);
+_putchar(',');
+_putchar(' ');
+_putchar('-');
+_putchar('1');
+_putchar('\n');
 }
+return (0);
 }
