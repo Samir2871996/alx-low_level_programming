ba12073d3bf9d77d7fb4ddd583ece241fef7b51a
Samir2871996 <samirmostafa1996@gmail.com>
2023-07-18 09:33:54 -0700

    sjhsjhjksdhsd

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints the time
+ *
+*/
+
+
+void jack_bauer(void)
+{
+	int i = '0';
+	int j = '0';
+	int k = '0';
+	int l = '1';
+
+
+	while (i <= '2')
+	{
+		while (j <= '9')
+		{
+			while (k <= '5')
+			{
+				while (l <= '9')
+				{
+					if ((i == '2') && (j == '3') && (k == '5') && (l == '9'))
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
+						_putchar('\n');
+						j = '9';
+
+					}
+					else 
+					{
+						_putchar(i);
+                                                _putchar(j);
+                                                _putchar(':');
+                                                _putchar(k);
+                                                _putchar(l);
+                                                _putchar('\n'); 
+                                                l++;
+					}
+				}
+
+				l = '0';
+				k++;
+			}
+
+			k = '0';
+			j++;
+		}
+
+		j = '0';
+		i++;
+	}
+}
