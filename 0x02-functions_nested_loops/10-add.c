25d47c869c7202c19b9dc95ec74b994adf49809a
Samir2871996 <samirmostafa1996@gmail.com>
2023-07-18 11:15:11 -0700

    hcdhsghjds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main,h"
+
+/**
+ * add - af unction that  dghd
+ *
+ * @a: fksakjlasjkfdjskldfs
+ * @b: kdjkjdsflkjdsfljkl
+ *
+ * Return: sum of the 2
+ */
+
+int add(int a, int b)
+{
+	sum = a + b;
+	return (sum);
+}
