d9c4814b2d4a492d289c7c795a0824d38522b334
Samir2871996 <samirmostafa1996@gmail.com>
2023-07-18 04:48:01 -0700

    lowrs

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Descriptio A c function that print the lowers
+ * Return: Always 0 (sucess)
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Descreption: A c function that prints thelower
+ *
+ * Return Always void
+*/
+
+void print_alphabet(void)
+{
+
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+
+		_putchar(i);
+	}
+
+}
