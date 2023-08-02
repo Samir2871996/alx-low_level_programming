61e8523da394038a9d0d13860cb5da8cace51cec
Samir2871996 <samirmostafa1996@gmail.com>
2023-08-02 11:53:01 -0700

    samir mostafa helmy

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		s++;
+		_puts_recursion(s);
+	}
+
+}
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+samir jhjshdajkhsdak
